af901c8900fb3f01b368565c215c6650215aefe4
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-03 23:50:50 +0100

    Create student class

---
+++ src/student.rb
@@ -0,0 +1,12 @@
+require './person.rb'
+
+class Student < Person
+  def initialize(id, name = "Unknown", age, parent_permission = true, classroom)
+    super(id, name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
