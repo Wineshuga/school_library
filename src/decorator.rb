f9c01d1476dbb08da9a8180f8a7ca4e46910cd9d
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-05 01:59:31 +0100

    Create decorator file and classes

---
+++ src/decorator.rb
@@ -0,0 +1,28 @@
+require "./nameable.rb"
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    nameable = @nameable.correct_name
+    if nameable.length > 10
+      nameable.slice(0, 10)
+    else
+      nameable
+    end
+  end
+end
